af7ef4823e087be314991f33c5a8fa8478de1d9d
minhhoang110927 <hoangminh.tran2139@gmail.com>
2026-04-23 19:51:31 +0700

    Update MainContent.tsx

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,82 +1,92 @@
 // src/components/MainContent.tsx
-import React from 'react';
-import './MainContent.css';
+import React from "react";
+import "./MainContent.css";
 
 // Dữ liệu cho phần Danh mục Sản phẩm (Hàng 1)
 const mainCategories = [
   {
-    title: 'Ceiling Recessed',
-    link: 'https://duragreenlighting.com.vn/product-category/ceiling-recessed/',
-    imgSrc: 'https://duragreenlighting.com.vn/wp-content/uploads/2024/04/CEILING-RECESSED.png',
+    title: "Diệu Thư",
+    link: "https://duragreenlighting.com.vn/product-category/ceiling-recessed/",
+    imgSrc:
+      "https://scontent.fsgn1-1.fna.fbcdn.net/v/t39.30808-6/618121084_1532536004713656_8023546562459392059_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=110&ccb=1-7&_nc_sid=7b2446&_nc_eui2=AeH8CeI8S1KnIs8n9iSvBy2sibIKIHuNyO6Jsgoge43I7nZywRxlSSN9snMs7RyDj3LvObMgjqU_kIH54nVs84KX&_nc_ohc=fE15fXy4T00Q7kNvwHPECoP&_nc_oc=AdqtVX9pxZ0rUuyfFFDqu48ignRvFxeolBduPWEtIVxo46MKetoo9VRyedmvfbjkQZs&_nc_zt=23&_nc_ht=scontent.fsgn1-1.fna&_nc_gid=9qvWWdFlVs3QW0fSVwGJDw&oh=00_Af1RwxcyIHWjoP3NjffC9a4cuHZMUNk29H2dwIEz14cYrg&oe=69EFB6D5",
   },
   {
-    title: 'Ceiling Surface',
-    link: 'https://duragreenlighting.com.vn/product-category/ceiling-surface/',
-    imgSrc: 'https://duragreenlighting.com.vn/wp-content/uploads/2024/04/CEILING-SURFACE.png',
+    title: "Ceiling Surface",
+    link: "https://duragreenlighting.com.vn/product-category/ceiling-surface/",
+    imgSrc:
+      "https://duragreenlighting.com.vn/wp-content/uploads/2024/04/CEILING-SURFACE.png",
   },
   {
-    title: 'Cu em',
-    link: 'https://duragreenlighting.com.vn/product-category/suspension/',
+    title: "CU em",
+    link: "https://duragreenlighting.com.vn/product-category/suspension/",
     // imgSrc: 'https://duragreenlighting.com.vn/wp-content/uploads/2024/04/SUSPENSTION.png',
-    imgSrc: 'https://scontent.fsgn1-1.fna.fbcdn.net/v/t39.30808-6/675316674_1528743258596116_1080220613491630171_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=a0f3c3&_nc_eui2=AeEHNjexNHHu3nVhveT93GR0heMNzWPMqkiF4w3NY8yqSBJ3X31VkCDw1WXApMYMMH4cHdQLK3JRhieaW0_jfG9F&_nc_ohc=h4zr29g9wdIQ7kNvwGgJC1r&_nc_oc=AdruG_9d7A7gNFz5wWwBVKJzSM0e-h4QZ38ufit4-B1SQ3efQyWUOgeDMglF8m-SvII&_nc_zt=23&_nc_ht=scontent.fsgn1-1.fna&_nc_gid=3Yy27_rTCF3ziCwAuEEUrQ&oh=00_Af0-DC-nh7YFxsZRdNCXqG1XhoOjMlSVybI4fgVw9qnkWw&oe=69EFB4A7'
+    imgSrc:
+      "https://scontent.fsgn1-1.fna.fbcdn.net/v/t39.30808-6/675316674_1528743258596116_1080220613491630171_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=a0f3c3&_nc_eui2=AeEHNjexNHHu3nVhveT93GR0heMNzWPMqkiF4w3NY8yqSBJ3X31VkCDw1WXApMYMMH4cHdQLK3JRhieaW0_jfG9F&_nc_ohc=h4zr29g9wdIQ7kNvwGgJC1r&_nc_oc=AdruG_9d7A7gNFz5wWwBVKJzSM0e-h4QZ38ufit4-B1SQ3efQyWUOgeDMglF8m-SvII&_nc_zt=23&_nc_ht=scontent.fsgn1-1.fna&_nc_gid=3Yy27_rTCF3ziCwAuEEUrQ&oh=00_Af0-DC-nh7YFxsZRdNCXqG1XhoOjMlSVybI4fgVw9qnkWw&oe=69EFB4A7",
   },
   {
-    title: 'Wall, Floor & Table',
-    link: 'https://duragreenlighting.com.vn/product-category/wall-floor-table/',
+    title: "Wall, Floor & Table",
+    link: "https://duragreenlighting.com.vn/product-category/wall-floor-table/",
     // imgSrc: 'https://duragreenlighting.com.vn/wp-content/uploads/2024/04/WALL-FLOOR.png',
-    imgSrc: 'https://scontent.fsgn1-1.fna.fbcdn.net/v/t39.30808-6/645427132_1477959250554430_2329477542097678824_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=7b2446&_nc_ohc=S403DKpTCogQ7kNvwHG5fCF&_nc_oc=Adr2bYXHN8kdLZgtWX3VIOSFDWf8PM6ruNTDJrbpGHSvS2WOLxoeaO6IPTVLixrdsvo&_nc_zt=23&_nc_ht=scontent.fsgn1-1.fna&_nc_gid=-I5M-tuJIJK5XnZyaflNkw&oh=00_Af1urOgzD2vdxh8AAi4LIjSRbDqvIIlEIuDpGdueAyQDFA&oe=69EFB406'
+    imgSrc:
+      "https://scontent.fsgn1-1.fna.fbcdn.net/v/t39.30808-6/645427132_1477959250554430_2329477542097678824_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=7b2446&_nc_ohc=S403DKpTCogQ7kNvwHG5fCF&_nc_oc=Adr2bYXHN8kdLZgtWX3VIOSFDWf8PM6ruNTDJrbpGHSvS2WOLxoeaO6IPTVLixrdsvo&_nc_zt=23&_nc_ht=scontent.fsgn1-1.fna&_nc_gid=-I5M-tuJIJK5XnZyaflNkw&oh=00_Af1urOgzD2vdxh8AAi4LIjSRbDqvIIlEIuDpGdueAyQDFA&oe=69EFB406",
   },
 ];
 
 // Dữ liệu cho phần Danh mục Sản phẩm (Hàng 2)
 const subCategories = [
   {
-    title: 'Outdoor',
-    link: 'https://duragreenlighting.com.vn/product-category/outdoor/',
-    imgSrc: 'https://duragreenlighting.com.vn/wp-content/uploads/2024/05/Outdoor-Projector-1-1024x1024.webp',
+    title: "Outdoor",
+    link: "https://duragreenlighting.com.vn/product-category/outdoor/",
+    imgSrc:
+      "https://duragreenlighting.com.vn/wp-content/uploads/2024/05/Outdoor-Projector-1-1024x1024.webp",
   },
   {
-    title: 'Track Lights',
-    link: 'https://duragreenlighting.com.vn/product-category/track-lights/',
-    imgSrc: 'https://duragreenlighting.com.vn/wp-content/uploads/2024/04/4022de5a99d4e3805c388fb97450ec9-1024x1024.png',
+    title: "Dửng Lơ",
+    link: "https://duragreenlighting.com.vn/product-category/track-lights/",
+    imgSrc:
+      "https://scontent.fsgn16-1.fna.fbcdn.net/v/t39.30808-6/638227163_2184485588757792_2065712790442312723_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=1d70fc&_nc_eui2=AeH51Q_e9M_ntVr919tzKyAjrZsCy3TrjhutmwLLdOuOG6bpH_W3pBJWaY3l_JNhekPfFTv42i9O2l9sH-deJAxF&_nc_ohc=XBSiHMVvutQQ7kNvwGb-EcW&_nc_oc=AdqXTUfHiGTEftpuQn_ubHZjU1_qepysUM6WlwlDp2RJzPWX4FY1d5f0V3sQ_CFuSgmum03JFsqOTmvaxpnKaR_A&_nc_zt=23&_nc_ht=scontent.fsgn16-1.fna&_nc_gid=bmhQajOlWhexHSRRXlA3yg&oh=00_Af0mpGVmCOdi4QD8zFQSdx0-Ur4FSwQeXoH-dcsBqw62Qw&oe=69EFE30B",
   },
   {
-    title: 'Diệu Thư',
-    link: 'https://duragreenlighting.com.vn/product-category/led-strip/',
-    // imgSrc: 'https://duragreenlighting.com.vn/wp-content/uploads/2024/05/111.png',
-    imgSrc: 'https://scontent.fsgn1-1.fna.fbcdn.net/v/t39.30808-6/618121084_1532536004713656_8023546562459392059_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=110&ccb=1-7&_nc_sid=7b2446&_nc_eui2=AeH8CeI8S1KnIs8n9iSvBy2sibIKIHuNyO6Jsgoge43I7nZywRxlSSN9snMs7RyDj3LvObMgjqU_kIH54nVs84KX&_nc_ohc=fE15fXy4T00Q7kNvwHPECoP&_nc_oc=AdqtVX9pxZ0rUuyfFFDqu48ignRvFxeolBduPWEtIVxo46MKetoo9VRyedmvfbjkQZs&_nc_zt=23&_nc_ht=scontent.fsgn1-1.fna&_nc_gid=9qvWWdFlVs3QW0fSVwGJDw&oh=00_Af1RwxcyIHWjoP3NjffC9a4cuHZMUNk29H2dwIEz14cYrg&oe=69EFB6D5'
+    title: "Led Strip",
+    link: "https://duragreenlighting.com.vn/product-category/led-strip/",
+    imgSrc:
+      "https://duragreenlighting.com.vn/wp-content/uploads/2024/05/111.png",
   },
   {
-    title: 'Accessories',
-    link: 'https://duragreenlighting.com.vn/product-category/control/',
-    imgSrc: 'https://duragreenlighting.com.vn/wp-content/uploads/2024/04/CONTROL.png',
+    title: "Accessories",
+    link: "https://duragreenlighting.com.vn/product-category/control/",
+    imgSrc:
+      "https://duragreenlighting.com.vn/wp-content/uploads/2024/04/CONTROL.png",
   },
 ];
 
 // Dữ liệu cho phần Instagram Gallery
 const instagramGallery = [
   {
-    link: '#',
-    imgSrc: 'https://duragreenlighting.com.vn/wp-content/uploads/2024/04/GARDEN.jpg',
+    link: "#",
+    imgSrc:
+      "https://duragreenlighting.com.vn/wp-content/uploads/2024/04/GARDEN.jpg",
   },
   {
-    link: '#',
-    imgSrc: 'https://duragreenlighting.com.vn/wp-content/uploads/2024/04/black-gift-box-1.jpg',
+    link: "#",
+    imgSrc:
+      "https://duragreenlighting.com.vn/wp-content/uploads/2024/04/black-gift-box-1.jpg",
   },
   {
-    link: '#',
-    imgSrc: 'https://duragreenlighting.com.vn/wp-content/uploads/2024/04/CRAFTSMANSHIP-1.jpg',
+    link: "#",
+    imgSrc:
+      "https://duragreenlighting.com.vn/wp-content/uploads/2024/04/CRAFTSMANSHIP-1.jpg",
   },
   {
-    link: '#',
-    imgSrc: 'https://duragreenlighting.com.vn/wp-content/uploads/2024/04/1713345932001.jpg',
+    link: "#",
+    imgSrc:
+      "https://duragreenlighting.com.vn/wp-content/uploads/2024/04/1713345932001.jpg",
   },
 ];
 
 const MainContent: React.FC = () => {
   return (
     <div className="main-content">
-
       {/* 1. Phần Danh Mục Sản Phẩm */}
       <section className="categories-section">
         {mainCategories.map((item, index) => (
@@ -113,14 +123,24 @@ const MainContent: React.FC = () => {
 
         <div className="instagram-gallery">
           {instagramGallery.map((item, index) => (
-            <a key={index} href={item.link} className="gallery-item" target="_blank" rel="noopener noreferrer">
-              <img src={item.imgSrc} alt={`Instagram post ${index + 1}`} className="gallery-img" loading="lazy" />
+            <a
+              key={index}
+              href={item.link}
+              className="gallery-item"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={item.imgSrc}
+                alt={`Instagram post ${index + 1}`}
+                className="gallery-img"
+                loading="lazy"
+              />
               <div className="gallery-overlay"></div>
             </a>
           ))}
         </div>
       </section>
-
     </div>
   );
 };
